e4d2efa22be1c5938f5d07a6cb1cfcab1781e34a
Rubén Garzón <rubengarzonzafra@gmail.com>
2022-04-23 19:20:16 +0200

    variables sass

--- src/app/pages/weather/weather.component.ts
+++ src/app/pages/weather/weather.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
-  styleUrls: ['./weather.component.scss']
+  styleUrls: ['./weather.component.sass'],
 })
 export class WeatherComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
