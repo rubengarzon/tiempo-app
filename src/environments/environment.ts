79341722b35255c02876bb497eb9dc6890bddfd2
Rubén Garzón <rubengarzonzafra@gmail.com>
2022-04-25 19:12:18 +0200

    environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   openWeather: {
-    key: '',
+    key: '3e88fd432e0e5d806ddb066007bf5314',
     url: 'https://api.openweathermap.org/data/2.5/',
   },
 };
